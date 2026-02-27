5d7acf6d40fd51cf2a876319831005170bbdaeff
sidharth <24cse405.sidharthpatnaik@giet.edu>
2026-02-27 12:07:29 +0530

    remain commits

--- backend/src/ECommerceAPI.Application/obj/Debug/net8.0/ECommerceAPI.Application.AssemblyInfo.cs
+++ backend/src/ECommerceAPI.Application/obj/Debug/net8.0/ECommerceAPI.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f0dfdb60f9d8fc21f3f14fb8fb8d1fec0406566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6aec51101d8ca8ffa8f0a0425371bbac49307da")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceAPI.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
